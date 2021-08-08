39b830071a072b1c1204587869c3f9ea260aa55b
KeyvanTheLegend <kayvan.yaghoubian@gmail.com>
2021-08-08 23:56:37 +0430

    init project

---
+++ Coconut/CoconutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoconutApp.swift
+//  Coconut
+//
+//  Created by sh on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct CoconutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

c32d565fbc7e4f9f538e5d91ea27f23ec4aa60f4
KeyvanTheLegend <kayvan.yaghoubian@gmail.com>
2021-08-09 18:56:31 +0430

    added Color+extension

---
+++ Coconut/Extentions/Color+extension.swift
@@ -0,0 +1,14 @@
+//
+//  Color+extension.swift
+//
+//  Created by Capitan on 8/9/21.
+
+import SwiftUI
+
+extension Color {
+    
+    static let background = Color("BackgroundColor")
+    static let white = Color("WhiteColor")
+    static let secondary = Color("BackgroundColor")
+
+}

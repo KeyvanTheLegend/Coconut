80c35590aa54d2cca45f35dd40e454949deeb233
KeyvanTheLegend <kayvan.yaghoubian@gmail.com>
2021-08-22 16:50:33 +0430

    Added Setting Tab View

---
+++ Coconut/Source Code/View/Setting Tab/SettingTabView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingTabView.swift
+//  Coconut
+//
+//  Created by sh on 8/22/21.
+//
+
+import SwiftUI
+
+struct SettingTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingTabView()
+    }
+}

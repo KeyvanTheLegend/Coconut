4bb76f5f24500c5e9d945619a576a638e82f70b3
KeyvanTheLegend <kayvan.yaghoubian@gmail.com>
2021-08-28 15:37:20 +0430

    fix tap to hide keyboard

--- Coconut/Source Code/View/SignIn/SignInView.swift
+++ Coconut/Source Code/View/SignIn/SignInView.swift
@@ -63,9 +63,6 @@ struct SignInView: View {
                         })
                     Spacer()
                 }
-                .onTapGesture {
-                    hideKeyboard()
-                }
                 /// Alert
                 .alert(isPresented: $viewModel.showAlert) {
                     Alert(
@@ -79,6 +76,9 @@ struct SignInView: View {
             }
             .background(Color.background.ignoresSafeArea())
             .ignoresSafeArea(.container)
+            .onTapGesture {
+                hideKeyboard()
+            }
         }
     }
 }
